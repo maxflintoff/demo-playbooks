93c5bb37c0733b7b82bcadfb524af3a0486de91d
Max Flintoff <max.flintoff@ibm.com>
2020-05-20 11:59:50 +0100

    adjusted terraform

--- infra/create-vm-ibmcloud.tf
+++ infra/create-vm-ibmcloud.tf
@@ -20,10 +20,23 @@ iaas_classic_username = var.iaas_classic_username
 iaas_classic_api_key  = var.iaas_classic_api_key
 }
 
+data "ibm_compute_ssh_key" "public_key" {
+    label = "${var.name_prefix}-ssh"
+}
+
+locals {
+    existing_key = var.ssh_key != data.ibm_compute_ssh_key.public_key.public_key
+}
+
 resource "ibm_compute_ssh_key" "ssh_key" {
+    count = local.existing_key ? 1 : 0
     label = "${var.name_prefix}-ssh"
     notes = "created automatically"
     public_key = var.ssh_key
+}
+
+locals {
+    ssh_key_id = local.existing_key ? ibm_compute_ssh_key.ssh_key[0].id : data.ibm_compute_ssh_key.public_key.id
 }
 
 resource "ibm_compute_vm_instance" "vm" {
@@ -33,7 +46,7 @@ resource "ibm_compute_vm_instance" "vm" {
   os_reference_code          = var.os
   datacenter                 = var.datacenter
   network_speed              = 1000
-  ssh_key_ids                = [ibm_compute_ssh_key.ssh_key.id]
+  ssh_key_ids                = [local.ssh_key_id]
   local_disk                 = false
   tags                       = var.tags
 }
